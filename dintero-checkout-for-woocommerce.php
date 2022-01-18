105555e73a20b54b7ec0066fa5d66dba6e4a319c
Montazar Al-Jaber <26507935+montazarr@users.noreply.github.com>
2022-01-18 11:52:02 +0100

    Bump WC version to current

--- dintero-checkout-for-woocommerce.php
+++ dintero-checkout-for-woocommerce.php
@@ -5,11 +5,11 @@
  * Description: Dintero Checkout for WooCommerce.
  * Author: Krokedil
  * Author URI: https://krokedil.com/
- * Version: 1.0.0
+ * Version: 0.1.0
  * Text Domain: dintero-checkout-for-woocommerce
  * Domain Path: /languages
  *
- * WC tested up to: 5.2.2
+ * WC requires at least: 6.1.0
  * WC tested up to: @TODO:
  *
  * Copyright (c) 2022 Krokedil
@@ -74,7 +74,7 @@ if ( ! class_exists( 'Dintero' ) ) {
 		 * Class constructor.
 		 */
 		public function __construct() {
-			 load_plugin_textdomain( 'dintero-checkout-for-woocommerce', false, plugin_basename( __DIR__ ) . '/languages' );
+			load_plugin_textdomain( 'dintero-checkout-for-woocommerce', false, plugin_basename( __DIR__ ) . '/languages' );
 			add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'plugin_action_links' ) );
 		}
 
@@ -122,6 +122,6 @@ if ( ! class_exists( 'Dintero' ) ) {
  *
  * @return Dintero
  */
-function Dintero() {                 // phpcs:ignore WordPress.NamingConventions.ValidFunctionName
+function Dintero() {                  // phpcs:ignore WordPress.NamingConventions.ValidFunctionName
 	return Dintero::get_instance();
 }
