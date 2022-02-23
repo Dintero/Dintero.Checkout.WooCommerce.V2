8384f514107dfdc052b3deca485e316e8931cf99
Montazar Al-Jaber <montazar@duck.com>
2022-02-23 13:53:27 +0100

    Add additional settings options

--- classes/class-dintero-checkout-settings-fields.php
+++ classes/class-dintero-checkout-settings-fields.php
@@ -22,80 +22,92 @@ class Dintero_Settings_Fields {
 	 */
 	public static function setting_fields() {
 		$settings = array(
-			'enabled'                    => array(
-				'title'       => __( 'Enable', 'dintero-checkout-for-woocommerce' ),
+			'enabled'                                 => array(
+				'title'       => __( 'Enable/Disable', 'dintero-checkout-for-woocommerce' ),
 				'label'       => __( 'Enable Dintero Checkout', 'dintero-checkout-for-woocommerce' ),
 				'type'        => 'checkbox',
 				'description' => '',
-				'default'     => 'no',
-			),
-			'title'                      => array(
-				'title'       => __( 'Title', 'dintero-checkout-for-woocommerce' ),
-				'type'        => 'text',
-				'description' => __( 'Payment method title.', 'Dintero-checkout-for-woocommerce' ),
-				'default'     => 'Dintero',
-				'desc_tip'    => true,
-			),
-			'description'                => array(
-				'title'       => __( 'Description', 'dintero-checkout-for-woocommerce' ),
-				'type'        => 'textarea',
-				'description' => __( 'Payment method description.', 'dintero-checkout-for-woocommerce' ),
-				'default'     => '',
-				'desc_tip'    => true,
-			),
-			'select_another_method_text' => array(
-				'title'             => __( 'Other payment method button text', 'dintero-checkout-for-woocommerce' ),
-				'type'              => 'text',
-				'description'       => __( 'Customize the <em>Select another payment method</em> button text that is displayed in checkout if using other payment methods than Dintero Checkout. Leave blank to use the default (and translatable) text.', 'dintero-checkout-for-woocommerce' ),
-				'default'           => '',
-				'desc_tip'          => true,
-				'custom_attributes' => array(
-					'autocomplete' => 'off',
-				),
-			),
-			'test_mode'                  => array(
-				'title'       => __( 'Test mode', 'dintero-checkout-for-woocommerce' ),
-				'label'       => __( 'Enable Test Mode', 'dintero-checkout-for-woocommerce' ),
-				'type'        => 'checkbox',
-				'description' => __( 'Place the payment gateway in test mode using test API keys.', 'dintero-checkout-for-woocommerce' ),
 				'default'     => 'yes',
-				'desc_tip'    => true,
 			),
-			'logging'                    => array(
-				'title'       => __( 'Logging', 'dintero-checkout-for-woocommerce' ),
-				'label'       => __( 'Log debug messages', 'dintero-checkout-for-woocommerce' ),
-				'type'        => 'checkbox',
-				'description' => __( 'Save debug messages to the WooCommerce System Status log (required for troubleshooting).', 'dintero-checkout-for-woocommerce' ),
-				'default'     => 'yes',
-				'desc_tip'    => true,
+
+			'dintero_configuration'                   => array(
+				'title' => __( 'Dintero configuration', 'dintero-checkout-for-woocommerce' ),
+				'type'  => 'title',
 			),
-			'profile_id'                 => array(
-				'title'       => __( 'Profile ID', 'dintero-checkout-for-woocommerce' ),
-				'type'        => 'text',
-				'default'     => '',
-				'desc_tip'    => true,
-				'description' => __( 'Test payment window profile ID. Found under (SETTINGS >> Payment windows) in Dintero Backoffice.', 'dinter-checkout-for-woocommerce' ),
-			),
-			'account_id'                 => array(
+			'account_id'                              => array(
 				'title'       => __( 'Account ID', 'dintero-checkout-for-woocommerce' ),
 				'type'        => 'text',
 				'default'     => '',
 				'desc_tip'    => true,
-				'description' => __( 'Found under (SETTINGS >> Account) in Dintero Backoffice.', 'dintero-checkout-for-woocommerce' ),
+				'description' => __( 'Found under (SETTINGS → Account) in Dintero Backoffice.', 'dintero-checkout-for-woocommerce' ),
 			),
-			'client_id'                  => array(
+			'client_id'                               => array(
 				'title'       => __( 'Client ID', 'dintero-checkout-for-woocommerce' ),
 				'type'        => 'text',
 				'default'     => '',
 				'desc_tip'    => true,
-				'description' => __( 'Generated under (SETTINGS >> API clients) in Dintero Backoffice.', 'dintero-checkout-for-woocommerce' ),
+				'description' => __( 'Generated under (SETTINGS → API clients) in Dintero Backoffice.', 'dintero-checkout-for-woocommerce' ),
 			),
-			'client_secret'              => array(
+			'client_secret'                           => array(
 				'title'       => __( 'Client secret', 'dintero-checkout-for-woocommerce' ),
 				'type'        => 'password',
 				'default'     => '',
 				'desc_tip'    => true,
-				'description' => __( 'Generated under (SETTINGS >> API clients) in Dintero Backoffice.', 'dintero-checkout-for-woocommerce' ),
+				'description' => __( 'Generated under (SETTINGS → API clients) in Dintero Backoffice.', 'dintero-checkout-for-woocommerce' ),
+			),
+			'profile_id'                              => array(
+				'title'       => __( 'Profile ID', 'dintero-checkout-for-woocommerce' ),
+				'type'        => 'text',
+				'default'     => 'default',
+				'desc_tip'    => true,
+				'description' => __( 'Test payment window profile ID. Found under (SETTINGS → Payment windows) in Dintero Backoffice.', 'dintero-checkout-for-woocommerce' ),
+			),
+			'test_mode'                               => array(
+				'title'       => __( 'Enable test mode:', 'dintero-checkout-for-woocommerce' ),
+				'label'       => ' ',
+				'type'        => 'checkbox',
+				'description' => __( 'While in test mode, the customers will NOT be charged. Useful for testing and debugging purposes.', 'dintero-checkout-for-woocommerce' ),
+				'default'     => 'no',
+				'desc_tip'    => true,
+			),
+			'logging'                                 => array(
+				'title'       => __( 'Enable logging', 'dintero-checkout-for-woocommerce' ),
+				'label'       => ' ',
+				'type'        => 'checkbox',
+				'description' => __( 'Required for troubleshooting. Log messages older than 30 days are automatically deleted.', 'dintero-checkout-for-woocommerce' ),
+				'default'     => 'yes',
+				'desc_tip'    => true,
+			),
+
+			'branding_title'                          => array(
+				'title' => __( 'Branding', 'dintero-checkout-for-woocommerce' ),
+				'type'  => 'title',
+			),
+			'branding_enable_logo'                    => array(
+				'title'   => __( 'Enable logos in footer', 'dintero-checkout-for-woocommerce' ),
+				'type'    => 'checkbox',
+				'default' => 'no',
+				'label'   => __( 'Enable', 'dintero-checkout-for-woocommerce' ),
+			),
+			'branding_logo_color'                     => array(
+				'title'   => __( 'Logo color', 'dintero-checkout-for-woocommerce' ),
+				'type'    => 'checkbox',
+				'default' => 'yes',
+				'label'   => __( 'Default color', 'dintero-checkout-for-woocommerce' ),
+			),
+			'branding_logo_color_custom'              => array(
+				'title' => __( 'Custom color', 'dintero-checkout-for-woocommerce' ),
+				'type'  => 'color',
+			),
+			'branding_footer_background_color'        => array(
+				'title'   => __( 'Footer background color', 'dintero-checkout-for-woocommerce' ),
+				'type'    => 'checkbox',
+				'default' => 'yes',
+				'label'   => __( 'Default color', 'dintero-checkout-for-woocommerce' ),
+			),
+			'branding_footer_background_color_custom' => array(
+				'title' => __( 'Custom color', 'dintero-checkout-for-woocommerce' ),
+				'type'  => 'color',
 			),
 		);
 
