dbfb8ccb8143ca4d3ffc0f693330f6bcf27f84cc
Montazar Al-Jaber <montazar@duck.com>
2022-03-21 17:49:05 +0100

    Add utility tools

--- gruntfile.js
+++ gruntfile.js
@@ -1,37 +1,32 @@
-module.exports = function (grunt) {
-	grunt.loadNpmTasks('grunt-wp-i18n');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-zip');
-	grunt.initConfig({
+module.exports = function( grunt ) {
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+	grunt.initConfig( {
 		makepot: {
 			target: {
 				options: {
 					domainPath: '/languages',
-					mainFile: 'dintero-checkout-for-woocommerce.php',
+					mainFile: 'dintero-checkout-for-woocommerce',
 					potFilename: 'dintero-checkout-for-woocommerce.pot',
-					processPot: function (pot, options) {
+					processPot( pot, options ) {
 						// add header options
 						return pot;
 					},
-					type: 'wp-plugin'
-				}
-			}
+					type: 'wp-plugin',
+				},
+			},
 		},
 		// minify css
 		cssmin: {
 			target: {
-				files: [{
+				files: [ {
 					expand: true,
 					cwd: './assets/css',
-					src: ['*.css', '!*.min.css'],
+					src: [ '*.css', '!*.min.css' ],
 					dest: './assets/css',
-					ext: '.min.css'
-				}]
-			}
+					ext: '.min.css',
+				} ],
+			},
 		},
-		// build release zip
-		zip: {
-			'./release/dintero-checkout-for-woocommerce.zip': [ 'readme.txt', 'assets/**/*', 'classes/**/*', 'includes/*', 'languages/*', 'templates/*', 'dintero-checkout-for-woocommerce.php' ]
-		}
-	});
-}
+	} );
+};
