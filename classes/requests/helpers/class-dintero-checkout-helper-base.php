33015c87f329b9cb85d619d3f9f8526a028bdfee
Montazar Al-Jaber <github.v0its@slmail.me>
2022-09-21 16:18:04 +0200

    Save shipping details as an array

--- classes/requests/helpers/class-dintero-checkout-helper-base.php
+++ classes/requests/helpers/class-dintero-checkout-helper-base.php
@@ -66,7 +66,7 @@ abstract class Dintero_Checkout_Helper_Base {
 				$body['express']['shipping_options'] = $helper->get_express_shipping_options();
 			} else {
 				$shipping_option                     = $helper->get_shipping_option();
-				$body['express']['shipping_options'] = empty( $shipping_option ) ? array() : $shipping_option;
+				$body['express']['shipping_options'] = empty( $shipping_option ) ? array() : array( $shipping_option );
 			}
 
 			$body['express']['shipping_mode'] = 'shipping_required';
