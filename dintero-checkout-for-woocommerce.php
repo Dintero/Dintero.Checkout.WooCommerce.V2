bf4941a2a591a35d2b97e1595a079835595fda76
Montazar <26507935+mntzrr@users.noreply.github.com>
2023-11-21 09:58:26 +0100

    Version 1.9.0

--- dintero-checkout-for-woocommerce.php
+++ dintero-checkout-for-woocommerce.php
@@ -5,12 +5,12 @@
  * Description: Dintero offers a complete payment solution. Simplifying the payment process for you and the customer.
  * Author: Dintero, Krokedil
  * Author URI: https://krokedil.com/
- * Version: 1.8.1
+ * Version: 1.9.0
  * Text Domain: dintero-checkout-for-woocommerce
  * Domain Path: /languages
  *
  * WC requires at least: 6.1.0
- * WC tested up to: 8.0.1
+ * WC tested up to: 8.2.1
  *
  * Copyright (c) 2023 Krokedil
  *
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DINTERO_CHECKOUT_VERSION', '1.8.1' );
+define( 'DINTERO_CHECKOUT_VERSION', '1.9.0' );
 define( 'DINTERO_CHECKOUT_URL', untrailingslashit( plugins_url( '/', __FILE__ ) ) );
 define( 'DINTERO_CHECKOUT_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'DINTERO_CHECKOUT_MAIN_FILE', __FILE__ );
