05be50b690cc671d752fdf4582e2fe9dc1ae2529
Montazar <github.v0its@slmail.me>
2024-06-19 10:33:47 +0200

    Refund by changing order status by default

--- classes/class-dintero-checkout-settings-fields.php
+++ classes/class-dintero-checkout-settings-fields.php
@@ -186,7 +186,7 @@ class Dintero_Settings_Fields {
 				'title'   => __( 'Refund by changing order status', 'dintero-checkout-for-woocommerce' ),
 				'label'   => __( 'Trigger refund in Dintero when WooCommerce order status is manually changed to <u>Refunded</u>.', 'dintero-checkout-for-woocommerce' ),
 				'type'    => 'checkbox',
-				'default' => 'no',
+				'default' => 'yes',
 			),
 			/* The Dintero Checkout Express Settings */
 			'dintero_checkout_express_settings_title' => array(
