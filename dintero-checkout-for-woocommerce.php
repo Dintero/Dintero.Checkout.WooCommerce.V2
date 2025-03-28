b231861587e8871735503ff9b5cab48af117a67e
anyakro <anya@krokedil.se>
2025-03-28 10:47:27 +0100

    Develop 0 decimal notice (#208)
    * Add admin notice if 0 decimals is used in wc
    
    * Update method description
    
    * Enable notice dissmiss
    
    * Prefix method & update classes/text

--- dintero-checkout-for-woocommerce.php
+++ dintero-checkout-for-woocommerce.php
@@ -223,6 +223,37 @@ if ( ! class_exists( 'Dintero' ) ) {
 			);
 
 			add_action( 'before_woocommerce_init', array( $this, 'declare_wc_compatibility' ) );
+
+			add_action( 'admin_notices', array( $this, 'dintero_wc_zero_decimal_notice' ) );
+		}
+
+
+		/**
+		 * Display a notice if the WooCommerce decimal setting is set to 0.
+		 *
+		 * @return void
+		 */
+		public function dintero_wc_zero_decimal_notice() {
+
+			if ( get_user_meta( get_current_user_id(), 'dismissed_dintero_wc_zero_decimal_notice', true ) ) {
+				return;
+			}
+
+			$decimals_setting = get_option( 'woocommerce_price_num_decimals', 2 );
+
+			if ( 0 == $decimals_setting ) {
+				$article_url = 'https://krokedil.com/dont-display-prices-with-0-decimals-in-woocommerce/';
+				?>
+				<div class="notice notice-warning woocommerce-message">
+						<a class="woocommerce-message-close notice-dismiss"
+							href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'wc-hide-notice', 'dintero_wc_zero_decimal' ), 'woocommerce_hide_notices_nonce', '_wc_notice_nonce' ) ); ?>">
+							<?php esc_html_e( 'Dismiss', 'woocommerce' ); ?>
+						</a>
+					<p><strong><?php esc_html_e( 'The decimal setting for prices in WooCommerce is currently set to 0.', 'dintero-checkout-for-woocommerce' ); ?></strong></p>
+					<p><?php esc_html_e( 'This may cause rounding issues with pricing and taxes. Learn more about why this can be problematic — and what to do instead: ', 'dintero-checkout-for-woocommerce' ); ?><a href="<?php echo esc_url( $article_url ); ?>" target="_blank"><?php esc_html_e( 'Don’t display prices with 0 decimals in WooCommerce', 'dintero-checkout-for-woocommerce' ); ?></a>.</p>
+				</div>
+				<?php
+			}
 		}
 
 		/**
